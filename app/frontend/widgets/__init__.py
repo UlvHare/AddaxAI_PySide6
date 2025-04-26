2df234428bdb90b7fa7fedce4a9762e2d6c6c691
UlvHare <61378569+UlvHare@users.noreply.github.com>
2025-04-26 09:09:54 +0300

    Delete app directory

--- app/frontend/widgets/__init__.py
+++
@@ -1,6 +0,0 @@
-# frontend/widgets/__init__.py
-from .range_slider import RangeSlider
-from .custom_table import CustomTable
-from .species_selector import SpeciesSelector
-
-# Add any additional imports for future widgets here
